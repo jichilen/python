ae42e1921a84cc9a6bfd5395029cd720ec791a2f
jichilen <lilikoi123@163.com>
2019-06-10 09:40:53 +0800

    Merge remote-tracking branch 'remotes/origin/master' bugs maybe found in all directories
    # Conflicts:
    #	mmdet/core/loss/__init__.py
    #	mmdet/core/loss/losses.py
    #	mmdet/models/backbones/__init__.py
    #	mmdet/models/detectors/__init__.py
    #	tools/test.py
    #	tools/train.py

---
+++ exercise/2_islands_num.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        两次遍历法
+        . x .
+        x - .
+        . . .
+        :param grid:
+        :return:
+        '''
+        
