725184982d29e6b1d75d3df19d62211dde566b4a
cyd <lilikoi123@163.com>
2019-06-16 14:15:47 +0800

    update

--- problem/7_construct_tree.py
+++ problem/7_construct_tree.py
@@ -4,39 +4,23 @@ class TreeNode():
         self.left= left
         self.right = right
 
-def printpre(head):
-    out = []
+def printtree(head):
+    outpre = []
+    outinter = []
+    outpos = []
     def dfs(node):
         if node == None:
             return None
-        out.append(node.val)
+        outpre.append(node.val)
         dfs(node.left)
+        outinter.append(node.val)
         dfs(node.right)
+        outpos.append(node.val)
     dfs(head)
-    print(out)
+    print(outpre)
+    print(outinter)
+    print(outpos)
 
-def printinter(head):
-    out = []
-
-    def dfs(node):
-        if node == None:
-            return
-        dfs(node.left)
-        out.append(node.val)
-        dfs(node.right)
-    dfs(head)
-    print(out)
-
-def printpos(head):
-    out = []
-    def dfs(node):
-        if node == None:
-            return
-        dfs(node.left)
-        dfs(node.right)
-        out.append(node.val)
-    dfs(head)
-    print(out)
 
 def constuct_tree(pre,ins):
     pass
@@ -51,8 +35,6 @@ if __name__ == '__main__':
     pre.left.right = TreeNode(8)
     pre.right.left = TreeNode(12)
     pre.right.right = TreeNode(16)
-    printpre(pre)
-    printinter(pre)
-    printpos(pre)
+    printtree(pre)
     presub = [1,2,4,7,3,5,6,8]
     insub = [4,7,2,1,5,3,8,6]
