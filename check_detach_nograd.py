8b195eddbcb1218920d16455f6eee3d1cd392b46
jichilen <lilikoi123@163.com>
2019-06-21 13:48:21 +0800

    commit

--- check_detach_nograd.py
+++ check_detach_nograd.py
@@ -14,7 +14,8 @@ class B(nn.Module):
     def __init__(self):
         super(B, self).__init__()
         W=torch.tensor([2.,2],requires_grad=True)
-        self.register_parameter('W', torch.nn.Parameter(W))
+        # self.register_parameter('W', torch.nn.Parameter(W))
+        self.W = torch.nn.Parameter(W)
 
     def forward(self, x):
         x = self.W * x
@@ -35,12 +36,28 @@ class C(nn.Module):
         x=self.B(x)
         return x
 
+class Bicubic(torch.autograd.Function):
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
+    def backward(self):
+        pass
+
+
+
 if __name__ == '__main__':
+    def bahook(moduel,input,output):
+        print(input)
+        print(output)
     c=torch.tensor([2,2.])
     net=C()
+    handle = net.A.register_backward_hook(bahook)
+
     d=net(c)
     f=d.sum()
     optimizer = torch.optim.SGD(net.parameters(), 0.02, momentum=0.9)
     optimizer.zero_grad()
     f.backward(retain_graph=True)
+    print(net.A.W.grad)
     optimizer.step()
