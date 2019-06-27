074a0c3b19387f89dcd1941fa1912201101f7d74
jichilen <lilikoi123@163.com>
2019-06-27 10:44:50 +0800

    minimum of reverse list

---
+++ problem/_11_reverse_list.py
@@ -0,0 +1,37 @@
+
+'''
+这是一个分治问题
+直接的来看需要二分，二分之后会有几种情况
+
+1.中间元素大于在三数中最大 [3,4,5,1,2] [3,4,5,6,7,1,2]
+2.中间元素在三数中最小 [4,5,1,2,3] [6,7,1,2,3,4,5]
+无论怎么分有一边一定是顺序的，这边没有最小值
+如何判断顺序？  第一个值小于最后一个值
+'''
+
+def reverse_list(nums,bg,ed):
+    #TODO: 这个例子有一个特例
+    # 说是递增，没有强调严格递增
+    # [1,0,1,1,1,1] 和 [1,1,1,1,0,1]总会出错一个
+    if len(nums)==0:
+        return None
+
+    mid = (bg+ed)//2
+    if nums[bg]<=nums[ed]:
+        return nums[bg]
+
+    if nums[mid]<nums[bg]:
+        return reverse_list(nums,bg,mid)
+    elif mid == bg:
+        return reverse_list(nums, mid+1, ed)
+    else:
+        return reverse_list(nums,mid,ed)
+
+
+
+if __name__ == '__main__':
+    nums = [1,2,3,4,5]
+    nums = [3,4,5,1,2]
+    nums = [1,1,1,1,0,1]
+    nu = reverse_list(nums,0,len(nums)-1)
+    print(nu)
